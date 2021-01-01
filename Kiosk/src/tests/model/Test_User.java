1706b33fec7d5be69d4cb7ea25a2b960a55aa3ac
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2021-01-01 12:49:11 +0000

    Wrote tests for User class

--- Kiosk/src/tests/model/Test_User.java
+++ Kiosk/src/tests/model/Test_User.java
@@ -1,7 +1,40 @@
 package tests.model;
+import java.lang.reflect.Field;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class Test_User {
+    // No need to test all setters for each model class, just test User.setUsername()
+
+    @Mock
+    UserManager mockUserManager;
+    @Test
+    public void test_setUsername_saveToFile(){
+        Field manager = UserManager.class.getDeclaredField("instance");
+        manager.set(null, mockUserManager);
+        User user = new User("JSmith", "12345678");
+        user.setUsername("JBrown");
+        Mockito.verify(mockUserManager).saveToFile();
+    }
+
+    @Mock
+    UsersController mockController;
+    @Test
+    public void test_setUsername_notifiesObserversOfUpdate(){
+        User user = new User("JSmith", "12345678");
+        user.register(mockController);
+        user.setUsername("JBrown");
+        Mockito.verify(mockController).updateViewUser();
+    }
+
+    @Test
+    public void test_notifyObserversOfDelete(){
+        User user = new User("JSmith", "12345678");
+        user.register(mockController);
+        user.notifyObserversOfDelete();
+        Mockito.verify(mockController).removeViewUser();
+    }
 }
