f60c2d691ffef5553cfd9eb41e7a49e3a8d75f8d
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2021-01-02 20:35:11 +0000

    Added comments to IModelManager to make it clearer what each method does

--- Kiosk/src/model/IModelManager.java
+++ Kiosk/src/model/IModelManager.java
@@ -3,7 +3,10 @@ package model;
 import controller.Controller;
 
 public interface IModelManager {
+    // Register an observer with all models being managed by the manager
     void register(Controller observer);
+    // De-register an observer from all the models being managed by the manager
     void remove(Controller observer);
+    // Save the models being managed to the file
     void saveToFile();
 }
