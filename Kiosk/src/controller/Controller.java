8fa27ff0feed66b93f017ff589ff87f279d9a800
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2021-01-02 11:24:49 +0000

    Added method stubs to Controller class

--- Kiosk/src/controller/Controller.java
+++ Kiosk/src/controller/Controller.java
@@ -1,4 +1,13 @@
 package controller;
 
+import model.User;
+
 public abstract class Controller {
+    public abstract void updateModel(int updateType, JsonObject newValue);
+    public abstract void updateViewUser(User updatedUser);
+    public abstract void updateViewStockItem(StockItem updatedItem);
+    public abstract void removeViewUser(User user);
+    public abstract void removeViewStockItem(StockItem item);
+    public abstract void removeViewOrder(Order order);
+    public void setView(AbstractView view);
 }
