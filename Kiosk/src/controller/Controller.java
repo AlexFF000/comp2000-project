f46367f7ef1d8e674d313821d0d9fe71fa7dc70e
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2021-01-05 11:59:23 +0000

    Added constants to Controller class.

--- Kiosk/src/controller/Controller.java
+++ Kiosk/src/controller/Controller.java
@@ -4,13 +4,36 @@ import model.User;
 import model.StockItem;
 import model.Order;
 import model.JsonObject;
+import view.AbstractView;
 
 public abstract class Controller {
+    // Constant parameters for updateModel's updateType argument
+    public static final int CREATE_USER = 0;
+    public static final int CREATE_STOCKITEM = 1;
+    public static final int CREATE_ORDER = 2;
+    public static final int DELETE_USER = 3;
+    public static final int DELETE_STOCKITEM = 4;
+    public static final int DELETE_ORDER = 5;
+    public static final int ORDER_DELIVERED = 6;
+    public static final int UPDATE_USERNAME = 7;
+    public static final int UPDATE_ITEM_BARCODE = 8;
+    public static final int UPDATE_PASSWORD = 9;
+    public static final int UPDATE_ITEM_NAME = 10;
+    public static final int UPDATE_ITEM_QUANTITY = 11;
+    public static final int UPDATE_REORDER_LEVEL = 12;
+    public static final int UPDATE_SALE_PRICE = 13;
+    public static final int UPDATE_SUPPLIER_PRICE = 14;
+
+    public AbstractView view;
+    
     public abstract void updateModel(int updateType, JsonObject newValue);
     public abstract void updateViewUser(User updatedUser);
     public abstract void updateViewStockItem(StockItem updatedItem);
     public abstract void removeViewUser(User user);
     public abstract void removeViewStockItem(StockItem item);
     public abstract void removeViewOrder(Order order);
-    //public void setView(AbstractView view);
+
+    public void setView(AbstractView view){
+        this.view = view;
+    }
 }
