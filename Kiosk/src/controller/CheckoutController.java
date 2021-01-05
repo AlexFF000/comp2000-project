e60484035ed1f7144af5e5289b1b3e813a5c30e7
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2021-01-05 15:38:45 +0000

    Implemented updateModel methods in CheckoutController

--- Kiosk/src/controller/CheckoutController.java
+++ Kiosk/src/controller/CheckoutController.java
@@ -2,12 +2,22 @@ package controller;
 
 import model.JsonObject;
 import model.StockItem;
+import model.StockManager;
+
+import java.util.ArrayList;
 
 public class CheckoutController extends Controller{
+    private ArrayList<StockItem> basket;
 
     @Override
     public void updateModel(int updateType, JsonObject newValue){
-
+        if (updateType == UPDATE_ITEM_QUANTITY){
+            // The only modification to the model that can be made by checkout is reducing item quantities
+            String barcode = newValue.getKey();
+            int newQuantity = newValue.getQuantityInStock();
+            StockItem item = StockManager.getInstance().getStockItem(barcode);
+            if (item != null) item.setQuantityInStock(newQuantity);
+        }
     }
 
     @Override
@@ -17,6 +27,30 @@ public class CheckoutController extends Controller{
 
     @Override
     public void removeViewStockItem(StockItem item){
+
+    }
+
+    public void scanItem(String barcode){
+
+    }
+
+    public void removeScannedItem(String barcode){
+
+    }
+
+    public void payCash(){
+
+    }
+
+    public void payCard(){
+
+    }
+
+    public void cancel(){
+
+    }
+
+    public void close(){
 
     }
 }
