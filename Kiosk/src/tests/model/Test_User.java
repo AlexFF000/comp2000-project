896a657146a5dafa3a39ebce33303136ff6a39e4
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2021-01-06 14:41:25 +0000

    Added two new tests to test User.passwordMatches

--- Kiosk/src/tests/model/Test_User.java
+++ Kiosk/src/tests/model/Test_User.java
@@ -5,6 +5,9 @@ import org.junit.runner.RunWith;
 import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 @RunWith(MockitoJUnitRunner.class)
 public class Test_User {
     // No need to test all setters for each model class, just test User.setUsername()
@@ -36,5 +39,17 @@ public class Test_User {
         user.register(mockController);
         user.notifyObserversOfDelete();
         Mockito.verify(mockController).removeViewUser();
+    }
+
+    @Test
+    public void test_passwordMatches_true(){
+        User user = new User("JSmith", "12345678");
+        assertTrue(user.passwordMatches("12345678"));
+    }
+
+    @Test
+    public void test_passwordMatches_false(){
+        User user = new User("JSmith", "12345678");
+        assertFalse(user.passwordMatches("12345679"));
     }
 }
