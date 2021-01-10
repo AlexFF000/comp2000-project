6ba93a9a34cd799bc2da6314a1717d28c49b1bf6
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2021-01-10 15:25:45 +0000

    Created ReceiptItem class for holding relevant details of purchased items that need to be added to receipt

---
+++ Kiosk/src/com/view/ReceiptItem.java
@@ -0,0 +1,11 @@
+package com.view;
+
+public class ReceiptItem {
+    public String name;
+    public float price;
+
+    public ReceiptItem(String name, float price){
+        this.name = name;
+        this.price = price;
+    }
+}
